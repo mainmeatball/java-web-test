036e700fcd0fdadbafa63368fca2fbe79b6b9889
sstepanov <sstepanov@solanteq.com>
2020-12-22 21:55:25 +0300

    Make controller singleton

--- src/main/java/com/sstep/http/controller/impl/HttpController.java
+++ src/main/java/com/sstep/http/controller/impl/HttpController.java
@@ -8,6 +8,24 @@ import com.sstep.http.annotation.Get;
  */
 public class HttpController extends AbstractHttpController {
 
+    private static volatile HttpController instance;
+
+    public static HttpController getInstance() {
+        HttpController localInstance = instance;
+        if (localInstance == null) {
+            synchronized (HttpController.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new HttpController();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    private HttpController() {
+    }
+
     @Get("/hello")
     public String hello() {
         return "HELLO!";
